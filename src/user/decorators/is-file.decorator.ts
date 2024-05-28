ff926897e1f9e3dfac52353dee33c2ae4f628518
Fesyse <maksimlol.mail.com@mail.ru>
2024-05-28 19:07:38 +0800

    chore: eslint/prettier plugin warnings warnings removed

--- src/user/decorators/is-file.decorator.ts
+++ src/user/decorators/is-file.decorator.ts
@@ -20,7 +20,8 @@ export function IsFile(
 			constraints: [],
 			options: validationOptions,
 			validator: {
-				validate(value: any, args: ValidationArguments) {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				validate(value: any, _args: ValidationArguments) {
 					if (
 						value?.mimetype &&
 						(options?.mime ?? []).includes(value?.mimetype)
